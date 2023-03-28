e974134b13f8de3f4e15f96796f73cad9c7d6276
Diwakar Adhikari <diwakar.adhikari@bajajfinserv.in>
2023-03-28 23:51:08 +0530

    sad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./landing/landing.module').then((m) => m.LandingModule),
+      import('./login/login.module').then((m) => m.LoginModule),
   },
   {
     path: 'contact-det',
